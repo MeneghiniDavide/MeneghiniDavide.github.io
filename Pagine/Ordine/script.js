a1c85851407cb5ae442e64b56aa62a3d313823e7
Davide Meneghini <toro.seduto@atomicmail.io>
2026-04-27 16:45:39 +0200

    Finito esercizio Ordine

--- Pagine/Ordine/script.js
+++ Pagine/Ordine/script.js
@@ -16,33 +16,72 @@ window.addEventListener('scroll', () => {
 
 function cambiaPrezzo1(){
     let quantita = document.getElementById("inputMouse").value;
-    let prezzoTotale = quantita*16;
-    document.getElementById("costoTotaleMouse").innerHTML = prezzoTotale;
+    if(quantita > 20){
+        let quantita = document.getElementById("inputMouse").value = 20;
+    }
+    else if(quantita < 0){
+        let quantita = document.getElementById("inputMouse").value = 0;
+    } else{
+        let prezzoTotale = quantita*16;
+        document.getElementById("costoTotaleMouse").innerHTML = prezzoTotale;
+    }
 }
 
 function cambiaPrezzo2(){
     let quantita = document.getElementById("inputTastiera").value;
-    let prezzoTotale = quantita*25;
-    document.getElementById("costoTotaleTastiera").innerHTML = prezzoTotale;
+    if(quantita > 20){
+        let quantita = document.getElementById("inputTastiera").value = 20;
+    }
+    else if(quantita < 0){
+        let quantita = document.getElementById("inputTastiera").value = 0;
+    } else{
+        let prezzoTotale = quantita*25;
+        document.getElementById("costoTotaleTastiera").innerHTML = prezzoTotale;
+    }
 }
 
 function cambiaPrezzo3(){
     let quantita = document.getElementById("inputStampante").value;
-    let prezzoTotale = quantita*350;
-    document.getElementById("costoTotaleStampante").innerHTML = prezzoTotale;
+    if(quantita > 20){
+        let quantita = document.getElementById("inputStampante").value = 20;
+    }
+    else if(quantita < 0){
+        let quantita = document.getElementById("inputStampante").value = 0;
+    } else{
+        let prezzoTotale = quantita*350;
+        document.getElementById("costoTotaleStampante").innerHTML = prezzoTotale;
+    }
 }
 
 function cambiaPrezzo4(){
     let quantita = document.getElementById("inputScanner").value;
-    let prezzoTotale = quantita*200;
-    document.getElementById("costoTotaleScanner").innerHTML = prezzoTotale;
+    if(quantita > 20){
+        let quantita = document.getElementById("inputScanner").value = 20;
+    }
+    else if(quantita < 0){
+        let quantita = document.getElementById("inputScanner").value = 0;
+    } else{
+        let prezzoTotale = quantita*200;
+        document.getElementById("costoTotaleScanner").innerHTML = prezzoTotale;
+    }
 }
 
 function elaboraOrdine(){
+
+    
     let p1 = parseFloat(document.getElementById("costoTotaleMouse").innerText) || 0;
     let p2 = parseFloat(document.getElementById("costoTotaleTastiera").innerText) || 0;
     let p3 = parseFloat(document.getElementById("costoTotaleStampante").innerText) || 0;
     let p4 = parseFloat(document.getElementById("costoTotaleScanner").innerText) || 0;
     let prezzoTotale = p1+p2+p3+p4;
     document.getElementById("costoTotale").innerHTML = prezzoTotale;
+}
+
+function visualizzaStringaFinale(){
+    let email = document.getElementById("email").value;
+    let pagamento = document.getElementById("menuPagamento").value;
+    let prezzo = document.getElementById("costoTotale").value;
+
+    document.getElementById("stringaFinale").innerHTML = "Grazie per il suo ordine di " + prezzo + "€, il pagamento avverrà tramite " + pagamento + ". Riceverà notifiche all’indirizzo " + email;
+
 }
